59834c21ae2ccb5fcac2a3641c6447eab9a869b5
OlegG <o.v.gamov@gmail.com>
2018-07-01 05:49:09 +0300

    added JSON output. changed the array to a map

--- Fibonacci/FibonGo.go
+++ Fibonacci/FibonGo.go
@@ -2,34 +2,43 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 )
 
 var (n int)
 
+
 func main() {
 
-	fib := [10]int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34,}
-	cor :=0
-	incor :=0
+	index := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"}
+	value := []interface{}{0,1,1,2,3,5,8,13,21,34,55,89,144}
 
-	for index, value  :=  range fib{
-		if cor !=10 && incor !=3{
+	m := make(map[string]interface{})
 
-		fmt.Println("Ввведите число: ")
-		fmt.Scanf("%d", &n)
-		if n == value {
-			fmt.Println("Welldone!")
-			cor += 1
-		} else {
-				fmt.Println("Wrong! \n", index, value)
+	cor := 0
+	incor := 0
+
+	for i, v  :=  range value {
+
+		if cor != 10 && incor != 3 {
+
+			fmt.Println("Ввведите число: ")
+			fmt.Scanf("%d", &n)
+
+			if n == v {
+				fmt.Println("Welldone!")
+				cor += 1
+
+			} else {
+				m[index[i]] = v
+				jsonString, _ := json.Marshal(m)
+				fmt.Printf("Wrong! \nCorrect is:  %s \n", jsonString)
 				incor += 1
+
 			}
 			fmt.Println("Correct", cor)
 			fmt.Println("Incorrect", incor)
 			fmt.Println("\n")
-			}
-
+		}
 	}
 }
-
-
